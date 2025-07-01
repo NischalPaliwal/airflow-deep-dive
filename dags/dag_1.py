b296127137a92b7fff0f38cf1f6a0b28a65806f8
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-07-01 15:25:01 +0530

    code added

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -5,9 +5,7 @@ from airflow.operators.python import PythonOperator
 default_args = {
     'owner': 'nischalpaliwal7',
     'retries': 4,
-    'retry_delay': timedelta(minutes=2),
-    'start_date': datetime(2025, 7, 1, 7),
-    'schedule': timedelta(weeks=1)
+    'retry_delay': timedelta(minutes=2)
 }
 
 def greet():
@@ -16,7 +14,9 @@ def greet():
 with DAG(
     dag_id='DAG002',
     default_args=default_args,
-    description="DAG using python operator!"
+    description="DAG using python operator!",
+    start_date=datetime(2025, 7, 1, 7),
+    schedule=timedelta(weeks=1)
 ) as dag:
     task1 = PythonOperator(
         task_id="T1",
