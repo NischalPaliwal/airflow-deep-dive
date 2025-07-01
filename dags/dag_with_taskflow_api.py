d8092251efc8ff931af3b2f9bc18edee8ab21b54
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-07-01 21:56:13 +0530

    code_added

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -7,6 +7,30 @@ default_args = {
     'retry_delay': timedelta(minutes=4)
 }
 
-@dag()
+@dag(dag_id="DAG_taskflowAPI",
+     default_args=default_args,
+     start_date=datetime(2025, 7, 1, 14),
+     schedule=timedelta(weeks=2)
+    )
 def pipeline():
-    pass
+
+    @task(multiple_outputs=True)
+    def get_name():
+        return {
+            'first_name': 'Lakshit',
+            'last_name': 'Paliwal'
+        }
+    
+    @task()
+    def get_age():
+        return 15
+    
+    @task()
+    def greet(first_name, last_name, age):
+        print(f"My name is {first_name} {last_name} and I am {age} years old.")
+    
+    name = get_name()
+    age = get_age()
+    greet(first_name=name['first_name'], last_name=name['last_name'], age=age)
+
+dag = pipeline()
